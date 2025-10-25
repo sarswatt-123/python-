a12a0d145f65fb113ecbe41f28723838cc296677
Teena <teenasaraswat04@gmail.com>
2025-10-25 18:50:26 +0530

    concept of data types in python

---
+++ data_types.py
@@ -0,0 +1,10 @@
+a=1  # a is a integer type variable 
+
+b = 2.5 # b is a float type variable
+
+c= "hello" # c is a string type variable
+
+d = None # d is a NoneType variable
+
+c=False # c is a boolean type variable
+
