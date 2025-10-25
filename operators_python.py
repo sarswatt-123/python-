f77f52638ad69102a22b6385323830df06c7510d
Teena <teenasaraswat04@gmail.com>
2025-10-25 18:57:36 +0530

    operators in python

---
+++ operators_python.py
@@ -0,0 +1,39 @@
+# operators in python 
+
+# Arithmetic Operators
+
+a=10 
+b=20
+print(a*b)
+print(a+b)
+print(b-a)
+print(b/a)
+print(b%a)
+print(b//a)
+print(a**2)
+
+# Assignment Operators
+c=5
+c+=2
+print(c)
+c*=3
+print(c)
+
+# Comparison Operators
+
+x=15
+y=10
+print(x>y)
+print(x<y)
+print(x==y)
+print(x!=y)
+
+# Logical Operators
+
+p=True
+q=False
+print(p and q)
+print(p or q)
+print(not p)
+
+
