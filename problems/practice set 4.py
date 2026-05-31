e3cc18dafd512ba4426caff82350aadf6f32d634
Teena <teenasaraswat04@gmail.com>
2026-05-31 07:18:55 +0530

    questions

---
+++ problems/practice set 4.py
@@ -0,0 +1,17 @@
+#Reverse a string 
+s= "teena"
+reverse=s[::-1]
+print(reverse)
+
+
+# Count a vowel
+s="education"
+print(s.count("e"))
+
+
+# Check palindrome 
+a="madam"
+if(a==a[::-1]):
+    print("yes palidrome ")
+else:
+    print("Not palindrome ")    
