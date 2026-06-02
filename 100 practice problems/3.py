e870f6b4121e2b6227dc46d92d3cd79429e53048
Teena <teenasaraswat04@gmail.com>
2026-06-02 20:53:42 +0530

    questions

--- 100 practice problems/3.py
+++ 100 practice problems/3.py
@@ -7,7 +7,9 @@ num2 = int(input("Enter the number 2 "))
 """(num1,num2) = (num2,num1)
 
 print("number 1 ",num1)
-print("number",num2)"""
+print("number",num2)
+
+"""
 
 #without using temporary variable 
 t=num1
---
+++ Loops concept/1.py
@@ -0,0 +1,15 @@
+for i in range(2,8):
+    print(i)
+print("end of for loop")
+
+i=1
+while(i<6):
+    print(i)
+    i=i+1
+print("end of while loop")
+
+l=["teena", "riya", "sumit",56,789,43,57,32,"vivek"]
+i=0
+while(i<len(l)):
+    print(l[i])
+    i=i+1
