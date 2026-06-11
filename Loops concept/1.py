79384c089844b581912082c86aab5463eecc6fd3
Teena <teenasaraswat04@gmail.com>
2026-06-11 10:02:21 +0530

    loops

--- Loops concept/1.py
+++ Loops concept/1.py
@@ -1,16 +1,21 @@
 for i in range(2,8):
     print(i)
-print("end of for loop")
+print("----------------------------")
 
 i=1
 while(i<6):
     print(i)
     i=i+1
-print("end of while loop")
+print("----------------------------------")
 
 l=["teena", "riya", "sumit",56,789,43,57,32,"vivek"]
 i=0
 while(i<len(l)):
     print(l[i])
     i=i+1
+print("----------------------------------- ")  
+
+S="Teena "
+for i in S:
+    print(i)
     
--- Loops concept/forloops.py
+++ Loops concept/forloops.py
@@ -1,2 +1,13 @@
 for i in range(0,8,2):
     print(i)
+print("------------------------")
+for i in range(50):
+    if i==10:
+        break
+    print(i)
+
+print("----------------------------")
+for i in range(50):
+    if i==10:
+        continue
+    print(i)
