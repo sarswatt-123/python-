53207e53a121fdd96482f4685a0ebaa3e8df3e79
Teena <teenasaraswat04@gmail.com>
2026-06-11 11:13:46 +0530

    questions

--- Loops concept/forloops.py
+++ Loops concept/forloops.py
@@ -11,3 +11,5 @@ for i in range(50):
     if i==10:
         continue
     print(i)
+
+    
---
+++ Loops concept/practice_questions.py
@@ -0,0 +1,29 @@
+"""n= int(input("Enter the number: "))
+for i in range(1,11):
+    print(i*n)
+    i=i+1
+print("------------------------------------------------------")
+
+
+
+
+a= int(input("Enter the number "))
+for i in range(1,11):
+    print(a,"*" ,  i  ,"=" ,a*i)
+    i=i+1
+
+
+
+#2. Write a program to greet all the person names stored in a list ‘l’ and which starts with S.
+
+l = ["Harry", "Soham", "Sachin", "Rahul"]
+for name in l:
+    if name.startswith("S"):
+        print("Hello",name)
+        
+"""
+
+
+
+
+
