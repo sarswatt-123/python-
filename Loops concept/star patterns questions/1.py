eab4e3f0fa07728270929ef99a65eec57d33eeb4
Teena <teenasaraswat04@gmail.com>
2026-06-12 07:41:32 +0530

    nested loop question

---
+++ Loops concept/star patterns questions/1.py
@@ -0,0 +1,17 @@
+""" star pattern 
+                      
+*****
+*****
+*****
+*****                          
+                                
+                                     
+ """
+
+for i in range(1,6):
+    for j in range(1,6):
+        print("*", end=" ")
+    print()
+
+
+
---
+++ Loops concept/star patterns questions/2.py
@@ -0,0 +1,15 @@
+"""
+
+*
+**
+***
+****
+*****
+
+"""
+
+r=int(input('Enter the number :'))
+for i in range(1,r+1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
