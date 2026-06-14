f736edbb6720ea5dfca4d1d1fac25bedddc8d897
Teena <teenasaraswat04@gmail.com>
2026-06-14 06:51:07 +0530

    calculator

---
+++ if_else conditional statement/calculator.py
@@ -0,0 +1,18 @@
+n1= int(input(" Enter the first number : "))
+n2= int(input("Enter the second number : "))
+
+op = input("Enter the operation ")
+if op == "+":
+    print("The addition of two numbers is ", n1+n2)
+elif op == "-":
+    print("The subtraction of two numbers is ", n1-n2)
+elif op == "*":
+    print("The multiplication of two numbers is ", n1*n2)
+elif op =="/":
+    print("The division  of two numbers is ", n1/n2)
+
+
+
+
+
+
