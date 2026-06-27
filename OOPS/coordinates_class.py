b4f21c9e2cb349ee5f0480bbc6191357dfa00542
Teena <teenasaraswat04@gmail.com>
2026-06-27 20:59:53 +0530

    coordinates class

---
+++ OOPS/coordinates_class.py
@@ -0,0 +1,56 @@
+"""
+
+Write OOP classes to handle the following scenarios:
+A user can create and view 2D coordinates
+A user can find out the distance between 2 coordinates
+A user can find find the distance of a coordinate from origin
+A user can check if a point lies on a given line
+A user can find the distance between a given 2D point and a given line
+
+"""
+
+class Point():
+    def __init__(self,x,y):
+        self.x_cod=x
+        self.y_cod=y
+
+    def __str__(self):
+        return "<{},{}>".format(self.x_cod,self.y_cod)
+    
+    def euclidian_distance(self,other):
+        return ((self.x_cod - other.y_cod)**2 + (self.y_cod - other.y_cod)**2)**0.5
+    
+    def distance_from_origin(self):
+        return self.euclidian_distance(Point(2,3))
+    
+class Line():
+    def __init__(self,A,B,C):
+        self.A=A
+        self.B=B
+        self.C=C
+    def __str__(self):
+        return "{}x +{}y +{} = 0".format(self.A,self.B,self.C)
+    
+    def point_on_line(line,point):
+        if line.A*point.x_cod + line.B*point.y_cod + line.C == 0:
+            return "lies on the line"
+        else:
+            return "does not lie on the line"
+
+l1 = Line(1,1,-2)
+p1 = Point(1,10)
+print(l1)
+print(p1)
+print(l1.point_on_line(p1))
+"""
+L1=Line(2,3,4)
+print(L1)   
+
+p1=Point(0,0)
+p2=Point(1,1)
+print(" coordinates of p1 : ",p1)
+print("Coordinates of p2",p2)
+print(p1.euclidian_distance(p2))
+print(p1.distance_from_origin())
+
+"""
--- OOPS/magic_methods.py
+++ OOPS/magic_methods.py
@@ -4,3 +4,16 @@ class Temp:
     def __init__(self):
         print("hello world ")
 obj=Temp()
+
+"""
+
+
+There are some meagic methods are - 
+ 1. __add__
+ 2. __sub__
+ 3. __mul__
+ 4. __truediv__
+
+
+
+"""
